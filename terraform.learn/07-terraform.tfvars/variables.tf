8401c03d9c3be6c006b4082727d3133bfc270c38
hassan_aftab <teo.devops@teo-intl.com>
2023-08-10 17:24:45 +0500

    tfvar file introduction

---
+++ terraform.learn/07-terraform.tfvars/variables.tf
@@ -0,0 +1,13 @@
+variable "usersage" {
+    type = map
+    default = {
+        hassan = 29
+        aftab = 19
+        thomas = 21
+    }
+}
+
+variable "username" {
+    type = string
+    default = "hassan"
+}
